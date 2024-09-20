2b044b8fd7683597d1352f28ddfdf8bee3619500
Arpan200502 <arpanmukherjee255@gmail.com>
2024-09-20 18:42:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,74 +1,74 @@
-var drum=document.querySelectorAll(".drum").length;
+var drum = document.querySelectorAll(".drum").length;
 
-for(var i=0;i<drum;i++){
-    
-    document.querySelectorAll(".drum")[i].addEventListener("click",gotclicked);
-function gotclicked(){
-    
-    var button = this.innerHTML;
-    Makesound(button);
-    ButtonAnimation(button);
-   
+// Loop to add event listeners to each drum button
+for (var i = 0; i < drum; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var button = this.innerHTML;
+        Makesound(button);
+        ButtonAnimation(button);
+    });
 }
 
-document.addEventListener("keydown",function(event){
-   Makesound(event.key);
-   ButtonAnimation(event.key);
-})
-}
+// Event listener for keyboard key presses (added once, outside the loop)
+document.addEventListener("keydown", function(event) {
+    Makesound(event.key);
+    ButtonAnimation(event.key);
+});
 
-
-
-
-function Makesound(key){
+// Function to play the sound based on the key/button
+function Makesound(key) {
     switch (key) {
         case "w":
-            var audio=new Audio("sounds/tom-1.mp3");
+            var audio = new Audio("sounds/tom-1.mp3");
             audio.volume = 0.2;
             audio.play();
-            
             break;
         case "a":
-            var audio=new Audio("sounds/tom-2.mp3");
+            var audio = new Audio("sounds/tom-2.mp3");
             audio.volume = 0.2;
             audio.play();
-            
             break;
         case "s":
-            var audio=new Audio("sounds/tom-3.mp3");
+            var audio = new Audio("sounds/tom-3.mp3");
             audio.volume = 0.2;
             audio.play();
             break;
         case "d":
-            var audio=new Audio("sounds/tom-4.mp3");
+            var audio = new Audio("sounds/tom-4.mp3");
             audio.volume = 0.2;
             audio.play();
             break;
         case "j":
-            var audio=new Audio("sounds/snare.mp3");
+            var audio = new Audio("sounds/snare.mp3");
             audio.volume = 0.2;
             audio.play();
             break;
-        case"k":
-        var audio=new Audio("sounds/kick-bass.mp3");
-        audio.volume = 0.2;
+        case "k":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.volume = 0.2;
             audio.play();
             break;
-        case"l":
-        var audio=new Audio("sounds/crash.mp3");
-        audio.volume = 0.2;
+        case "l":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.volume = 0.2;
             audio.play();
             break;
         default:
+            console.log("Invalid key: " + key);
             break;
     }
 }
 
-function ButtonAnimation(currentKey){
-  var activeButton= document.querySelector("." + currentKey);
- activeButton.classList.add("pressed");
+// Function to add and remove animation on button press
+function ButtonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
 
- setTimeout(function(){
-    activeButton.classList.remove("pressed");
- },100);
+    // Check if the activeButton exists before applying the animation
+    if (activeButton) {
+        activeButton.classList.add("pressed");
+
+        setTimeout(function() {
+            activeButton.classList.remove("pressed");
+        }, 100);
+    }
 }
